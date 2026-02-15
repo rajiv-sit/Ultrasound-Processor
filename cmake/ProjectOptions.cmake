ddae52fc34d37398647edd0a4293a1d558281152
Rajiv <rajiv.sithiravel@gmail.com>
2026-02-15 16:27:17 -0500

    chore: remove cmake and codex from tracked files

--- cmake/ProjectOptions.cmake
+++
@@ -1,8 +0,0 @@
-include_guard(GLOBAL)
-
-if(MSVC)
-    add_compile_options(/W4 /permissive-)
-    add_compile_definitions(_CRT_SECURE_NO_WARNINGS)
-else()
-    add_compile_options(-Wall -Wextra -Wpedantic)
-endif()
